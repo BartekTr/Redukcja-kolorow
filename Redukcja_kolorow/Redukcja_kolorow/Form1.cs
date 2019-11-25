51d14c2dd2e01154a285f1664575b32864c7b951
290510 (Bartłomiej Truszkowski) <01130643@pw.edu.pl>
2019-11-25 01:18:02 +0100

    Creating project with basic functionality

--- Redukcja_kolorow/Redukcja_kolorow/Form1.cs
+++ Redukcja_kolorow/Redukcja_kolorow/Form1.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,79 @@ namespace Redukcja_kolorow
 {
     public partial class Form1 : Form
     {
+        Bitmap pictureTexture, bmp2, bmp3;
+        Color[,] colors;
+        Octree tree;
         public Form1()
         {
             InitializeComponent();
+            pictureTexture = new Bitmap("example2.bmp");
+            using (pictureTexture)
+            {
+                bmp2 = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                bmp3 = new Bitmap(pictureBox2.Width, pictureBox2.Height);
+                using (var g = Graphics.FromImage(bmp2))
+                {
+                    g.InterpolationMode = InterpolationMode.NearestNeighbor;
+                    g.DrawImage(pictureTexture, new Rectangle(Point.Empty, bmp2.Size));
+                    pictureBox1.Image = bmp2;
+                }
+                using (var g = Graphics.FromImage(bmp3))
+                {
+                    g.InterpolationMode = InterpolationMode.NearestNeighbor;
+                    g.DrawImage(pictureTexture, new Rectangle(Point.Empty, bmp3.Size));
+                    colors = GetColorsFromBitmap(bmp3, pictureBox2.Width, pictureBox2.Height);
+                }
+            }
+            InitTree();
         }
+
+        private void InitTree()
+        {
+            tree = new Octree();
+            for (int i = 0; i < pictureBox2.Width; i++)
+                for (int j = 0; j < pictureBox2.Height; j++)
+                {
+                    tree.InsertTree(new BinaryColor(colors[i, j]), 0);
+                }
+        }
+
+        private void pictureBox2_Paint(object sender, PaintEventArgs e)
+        {
+            Bitmap temporaryBitmap = new Bitmap(pictureBox2.Width, pictureBox2.Height);
+            unsafe
+            {
+                BitmapData bitmapData = temporaryBitmap.LockBits(new Rectangle(0, 0, pictureBox2.Width, pictureBox2.Height), ImageLockMode.ReadWrite, temporaryBitmap.PixelFormat);
+                int heightInPixels = bitmapData.Height;
+                int bytesPerPixel = Bitmap.GetPixelFormatSize(temporaryBitmap.PixelFormat) / 8;
+                int widthInBytes = bitmapData.Width * bytesPerPixel;
+                byte* firstPixel = (byte*)bitmapData.Scan0;
+
+                Parallel.For(0, heightInPixels, y =>
+                {
+                    byte* currentLine = firstPixel + (y * bitmapData.Stride);
+                    for (int x = 0; x < widthInBytes; x = x + bytesPerPixel)
+                    {
+                        currentLine[x] = colors[x / 4, y].B;
+                        currentLine[x + 1] = colors[x / 4, y].G;
+                        currentLine[x + 2] = colors[x / 4, y].R;
+                        currentLine[x + 3] = colors[x / 4, y].A;
+                    }
+                });
+                temporaryBitmap.UnlockBits(bitmapData);
+            }
+            e.Graphics.DrawImage(temporaryBitmap, 0, 0);
+
+        }
+
+        private Color[,] GetColorsFromBitmap(Bitmap bmp, int x, int y)
+        {
+            Color[,] colors = new Color[x, y];
+            for (int i = 0; i < x; i++)
+                for (int j = 0; j < y; j++)
+                    colors[i, j] = bmp.GetPixel(i, j);
+            return colors;
+        }
+
     }
 }
---
+++ Redukcja_kolorow/Redukcja_kolorow/Octree.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Redukcja_kolorow
+{
+    public class BinaryColor
+    {
+        public BitArray R;
+        public BitArray G;
+        public BitArray B;
+
+        public BinaryColor(Color color)
+        {
+            R = new BitArray(BitConverter.GetBytes(color.R));
+            G = new BitArray(BitConverter.GetBytes(color.G));
+            B = new BitArray(BitConverter.GetBytes(color.B));
+        }
+        public Color GetColor()
+        {
+            var r = new int[1];
+            var g = new int[1];
+            var b = new int[1];
+            R.CopyTo(r, 0);
+            G.CopyTo(g, 0);
+            B.CopyTo(b, 0);
+            return Color.FromArgb(r[0],g[0],b[0]);
+        }
+    }
+    public class Octree
+    {
+        public int color_count = 0;
+        public Color color;
+        public Octree[] Next;
+        public Octree()
+        {
+            Next = new Octree[8];
+        }
+        public void InsertTree(BinaryColor color, int depth)
+        {
+            if (depth >= 8)
+            {
+                this.color = color.GetColor();
+                color_count++;
+                Next = null;
+            }
+            else
+            {
+                Next[Branch(color.R[depth], color.G[depth], color.B[depth])] = new Octree();
+                Next[Branch(color.R[depth], color.G[depth], color.B[depth])]
+                    .InsertTree(color, depth + 1);
+            }
+        }
+        private int Branch(bool R, bool G, bool B)
+        {
+            int res = 0;
+            res += R ? 1 : 0;
+            res += G ? 2 : 0;
+            res += B ? 4 : 0;
+            return res;
+        }
+    }
+
+}
